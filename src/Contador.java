ffbdbe1496f89c40eb934368badd176975e62a61
marielless <mariellesantos96@hotmail.com>
2025-02-13 01:22:07 -0300

    Criação do projeto implementando controle de fluxo com tratamento de erro.

---
+++ src/Contador.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+// Classe de exceção customizada
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            // Exibir mensagem de erro quando o primeiro parâmetro for maior
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é maior que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        // Loop para imprimir os números
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
